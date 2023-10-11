f0f98f25c793e285efd4208327eb99d91e526463
cezarmello <138020747+cezarmello@users.noreply.github.com>
2023-10-11 14:14:18 +0000

    bbc

--- diadoprofessor-main/main.js
+++ diadoprofessor-main/main.js
@@ -3,11 +3,15 @@ document.addEventListener("DOMContentLoaded", function () {
     var mostrarMensagemBtn = document.getElementById("mostrarMensagem");
 
     mostrarMensagemBtn.addEventListener("click", function () {
-        mensagem.innerHTML = "Neste Dia dos Professores, quero expressar minha mais profunda gratidão por tudo o que vocês fazem. " +
+        mensagem.innerHTML = "Queridos professores, hoje é o dia de celebrar vocês, " +
+        "os verdadeiros heróis que moldam mentes e inspiram sonhos. " +
+        "Em cada sala de aula, vocês desempenham um papel vital na formação do futuro. " +
         "Seu comprometimento, paixão e dedicação são admiráveis e dignos de reconhecimento. " +
-        "Os verdadeiros heróis que moldam mentes e inspiram sonhos." +
-        "Ser professor é um trabalho que exige esforço, garra, paciência e muita empatia. " +
-        "Muita gratidão por todos os profissionais da educação no dia de hoje. " 
+        "Vocês não apenas compartilham conhecimento, mas também cultivam valores, orientam e apoiam os sonhos de seus alunos. " +
+        "Com paciência infinita, vocês transformam desafios em oportunidades de aprendizado e capacitam os jovens a serem cidadãos responsáveis e pensadores críticos. " +
+        "Neste Dia dos Professores, quero expressar minha mais profunda gratidão por tudo o que vocês fazem. " +
+        "Vocês são faróis de luz na jornada educacional de todos nós, iluminando o caminho com sabedoria e compreensão. " +
+        "Obrigado por inspirarem, orientarem e ajudarem a construir um futuro melhor para todos. " +
         "Feliz Dia dos Professores!";
     });
 });
